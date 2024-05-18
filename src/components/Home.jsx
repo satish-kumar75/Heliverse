5da780f8e7e1ee7e2fa752cf063357f00e390013
satsihkumar75 <Satishk404456@gmail.com>
2024-05-18 11:19:01 +0530

    Fix the animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,84 +4,137 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(useGSAP, ScrollTrigger);
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
 
-const useGsapAnimations = (refs) => {
-  useGSAP(() => {
-    gsap.from(".hero-contaienr .left, .hero-contaienr .right", {
-      y: 390,
-      duration: 1.4,
-      opacity: 0,
-    });
-  }, [refs[0]]);
+const Home = () => {
+  const container1 = useRef(null);
+  const container2 = useRef(null);
+  const container3 = useRef(null);
+  const container4 = useRef(null);
+  const container5 = useRef(null);
+  const container6 = useRef(null);
 
-  const scrollTriggerSettings = [
-    {
-      ref: refs[1],
-      x: 0,
-      y: 200,
-      trigger: "rating-img",
-      start: "25% 93%",
-      end: "30% 90%",
+  useGSAP(
+    () => {
+      gsap.from(".hero-contaienr .left", {
+        y: 390,
+        duration: 1.4,
+        opacity: 0,
+      });
+      gsap.from(".hero-contaienr .right", {
+        y: 390,
+        duration: 1.4,
+        opacity: 0,
+      });
     },
-    {
-      ref: refs[2],
-      x: -200,
-      y: 0,
-      trigger: "rating-img",
-      start: "30% 93%",
-      end: "35% 90%",
-    },
-    {
-      ref: refs[3],
-      x: -200,
-      y: 0,
-      trigger: "rating-img",
-      start: "86% 93%",
-      end: "98% 90%",
-    },
-    {
-      ref: refs[4],
-      x: 200,
-      y: 0,
-      trigger: "rating-img",
-      start: "65% 93%",
-      end: "75% 90%",
-    },
-    {
-      ref: refs[5],
-      x: -400,
-      y: 0,
-      trigger: "rating-img",
-      start: "40% 93%",
-      end: "60% 90%",
-    },
-  ];
+    { scope: container1 }
+  );
 
-  scrollTriggerSettings.forEach(({ ref, x, y, trigger, start, end }) => {
-    useGSAP(() => {
-      gsap.from(ref.current, {
-        x,
-        y,
+  useGSAP(
+    () => {
+      gsap.from(container2.current, {
+        y: 200,
         duration: 1.4,
         stagger: 0.3,
         opacity: 0,
         scrollTrigger: {
-          trigger,
-          start,
-          end,
+          trigger: "rating-img",
+          start: "25% 93%",
+          end: "30% 90%",
           scrub: true,
           yoyo: true,
         },
       });
-    }, [ref]);
-  });
-};
+    },
+    { scope: container2 }
+  );
+  useGSAP(
+    () => {
+      gsap.from(".left", {
+        x: -200,
+        duration: 1.4,
+        stagger: 0.3,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: "rating-img",
+          start: "30% 93%",
+          end: "35% 90%",
+          scrub: true,
+          yoyo: true,
+        },
+      });
+      gsap.from(".right", {
+        x: 200,
+        duration: 1.4,
+        stagger: 0.3,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: "rating-img",
+          start: "30% 93%",
+          end: "35% 90%",
+          scrub: true,
+          yoyo: true,
+        },
+      });
+    },
+    { scope: container3 }
+  );
 
-const Home = () => {
-  const containerRefs = Array.from({ length: 6 }, () => useRef(null));
-
-  useGsapAnimations(containerRefs);
+  useGSAP(
+    () => {
+      gsap.from(container4.current, {
+        x: -200,
+        duration: 1.4,
+        stagger: 0.3,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: "rating-img",
+          start: "86% 93%",
+          end: "98% 90%",
+          scrub: true,
+          yoyo: true,
+        },
+      });
+    },
+    { scope: container4 }
+  );
+  useGSAP(
+    () => {
+      gsap.from(container5.current, {
+        x: 200,
+        duration: 1.4,
+        stagger: 0.3,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: "rating-img",
+          start: "65% 93%",
+          end: "75% 90%",
+          scrub: true,
+          yoyo: true,
+        },
+      });
+    },
+    { scope: container5 }
+  );
+  useGSAP(
+    () => {
+      gsap.from(container6.current, {
+        x: -400,
+        duration: 1.4,
+        stagger: 0.3,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: "rating-img",
+          start: "40% 93%",
+          end: "60% 90%",
+          scrub: true,
+          yoyo: true,
+        },
+      });
+    },
+    { scope: container6 }
+  );
 
   const features = [
     {
@@ -102,34 +155,11 @@ const Home = () => {
     },
   ];
 
-  const ratingItems = [
-    { img: assets.img2, score: "4.5 Score, 9 Reviews" },
-    { img: assets.img1, score: "4.5 Score, 9 Reviews" },
-    { img: assets.img3, score: "4.5 Score, 9 Reviews" },
-  ];
-
-  const applyItems = [
-    {
-      title: "Apply On Section",
-      description:
-        "Apply on section is a game-changer, offering an unparalleled way to manage applications directly from your website.",
-      imgSrc: assets.img11,
-      extraClass: "",
-    },
-    {
-      title: "Apply On Page",
-      description:
-        "Take your website to new heights with Motion Art for Elementor. Embrace the power of motion and animation.",
-      imgSrc: assets.img10,
-      extraClass: "sm:mt-20",
-    },
-  ];
-
   return (
     <div className="max-w-screen-xl mx-auto px-3 overflow-hidden">
       <div className="header flex justify-between pt-9 items-center">
         <a href="">
-          <img src={assets.logo} alt="motion art logo" />
+          <img loading="lazy" src={assets.logo} alt="motion art logo" />
         </a>
         <a
           className="sm:block hidden bg-white text-black px-8 py-3 font-medium rounded-lg inline-block h-fit border-2 hover:border-white hover:text-white hover:bg-transparent duration-300 ease-in-out"
@@ -141,7 +171,7 @@ const Home = () => {
       </div>
 
       <div
-        ref={containerRefs[0]}
+        ref={container1}
         className="hero-contaienr overflow-hidden grid grid-cols-1 md:grid-cols-12 max-w-7xl mx-auto py-20 gap-4 justify-items-center sm:justify-items-start sm:text-start text-center"
       >
         <div className="left md:col-span-3">
@@ -154,7 +184,7 @@ const Home = () => {
           <span className="sm:max-w-36 block">With Motion Art Effect</span>
         </div>
         <div className="right md:col-span-6">
-          <h1 className="leading-[80px] font-sora font-medium mb-5">
+          <h1 className="leading-[80px] font-sora font-medium mb-5 ">
             Attract Your Visitors Attention With Colorful <br />
             <span className="bg-clip-text text-transparent bg-gradient1 font-semibold">
               Motion Art Effect
@@ -174,14 +204,18 @@ const Home = () => {
           Trusted by thousands of users around the world
         </h3>
         <div
-          ref={containerRefs[1]}
+          ref={container2}
           className="overflow-hidden flex lg:gap-24 gap-10 pt-12 flex-wrap lg:justify-between justify-center"
         >
-          {ratingItems.map((item, index) => (
+          {[
+            { img: assets.img2, score: "4.5 Score, 9 Reviews" },
+            { img: assets.img1, score: "4.5 Score, 9 Reviews" },
+            { img: assets.img3, score: "4.5 Score, 9 Reviews" },
+          ].map((item, index) => (
             <div key={index} className="flex items-center gap-3">
-              <img src={item.img} alt="" />
+              <img loading="lazy" src={item.img} alt="" />
               <div>
-                <img src={assets.img4} alt="" />
+                <img loading="lazy" src={assets.img4} alt="" />
                 <p className="mt-4 text-zinc-400">{item.score}</p>
               </div>
             </div>
@@ -190,7 +224,7 @@ const Home = () => {
       </div>
 
       <div
-        ref={containerRefs[2]}
+        ref={container3}
         className="cursor-magic grid grid-cols-1 md:grid-cols-5 gap-10 pt-28 justify-items-center md:justify-items-end sm:text-start text-center"
       >
         <div className="md:col-span-3">
@@ -214,16 +248,31 @@ const Home = () => {
           </div>
         </div>
         <div className="md:col-span-2 right">
-          <img src={assets.img5} alt="" />
+          <img loading="lazy" src={assets.img5} alt="" />
         </div>
       </div>
 
-      <div ref={containerRefs[5]} className="apply pt-28">
+      <div ref={container6} className="apply pt-28">
         <h2 className="text-center max-w-2xl mx-auto">
           Apply On Any Section Or Enable For Whole Page
         </h2>
         <div className="flex gap-4 flex-wrap sm:flex-nowrap mt-14">
-          {applyItems.map((item, index) => (
+          {[
+            {
+              title: "Apply On Section",
+              description:
+                "Apply on section is a game-changer, offering an unparalleled way to manage applications directly from your website.",
+              imgSrc: assets.img11,
+              extraClass: "",
+            },
+            {
+              title: "Apply On Page",
+              description:
+                "Take your website to new heights with Motion Art for Elementor. Embrace the power of motion and animation.",
+              imgSrc: assets.img10,
+              extraClass: "sm:mt-20",
+            },
+          ].map((item, index) => (
             <div
               key={index}
               className={`bg-gradient3 border-2 border-[#251E35] rounded-3xl p-8 max-w-[610px] h-fit ${item.extraClass}`}
@@ -233,9 +282,10 @@ const Home = () => {
                 {item.description}
               </p>
               <img
+                loading="lazy"
                 className="max-w-full h-auto"
                 src={item.imgSrc}
-                alt={item.title}
+                alt=""
               />
             </div>
           ))}
@@ -243,7 +293,7 @@ const Home = () => {
       </div>
 
       <div
-        ref={containerRefs[4]}
+        ref={container5}
         className="browser bg-gradient3 border-2 border-[#251E35] rounded-3xl sm:mt-32 mt-6 px-10 py-12"
       >
         <div className="max-w-[460px] mx-auto text-center">
@@ -252,11 +302,11 @@ const Home = () => {
             Rest assured, Motion Art is designed to be compatible with all major
             web browsers.
           </p>
-          <img src={assets.img8} alt="" />
+          <img loading="lazy" src={assets.img8} alt="" />
         </div>
       </div>
 
-      <div ref={containerRefs[3]} className="apply my-28">
+      <div ref={container4} className="apply my-28">
         <div className="text-center max-w-2xl mx-auto">
           <h2>An All-Round Plugin With Powerful Features</h2>
           <p className="text-zinc-400 mt-3">
@@ -271,7 +321,7 @@ const Home = () => {
               key={index}
               className="feature bg-gradient3 border-2 border-[#251E35] rounded-3xl sm:max-w-[400px] w-full"
             >
-              <img src={item.imgSrc} alt={item.title} />
+              <img loading="lazy" src={item.imgSrc} alt={item.title} />
               <h2 className="px-9 pt-6 mb-3">{item.title}</h2>
               <p className="px-9 pb-10 text-zinc-400">{item.description}</p>
             </div>
