14436dca26268a96ce4235f69ca9d0b4fbeffcb4
satsihkumar75 <Satishk404456@gmail.com>
2024-05-18 11:00:55 +0530

    updated readme file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import initFluid from "./script";
 
 const App = () => {
+  useEffect(() => {
+    initFluid();
+  }, []);
   return (
     <div>
       <Home />
